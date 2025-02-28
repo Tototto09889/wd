e6230eb2021bd8fd86793f35b48cae944b14f6e4
Tototto09889 <74698145+Tototto09889@users.noreply.github.com>
2025-02-28 22:27:43 +0800

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,21 @@
+// Ini src/content.config.ts
+
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  loader: async ({ glob }) => {
-    const posts = await glob("./src/content/blog/**/*.{md,mdx}");
-    const nestedPosts = await glob("./src/content/blog/*/*/*.{md,mdx}");
-    return [...posts, ...nestedPosts]; // Sudah benar
-  },
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
-    isNested: z.boolean().optional(),
   }),
 });
 
 export const collections = { blog };
+
